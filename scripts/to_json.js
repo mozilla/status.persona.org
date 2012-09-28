92df87df5e70537f01b5416910ce4d4422ec23ef
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-09-28 08:33:00 -0600

    have to_json.js write to file

--- scripts/to_json.js
+++ scripts/to_json.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 var event_parser = require('./lib/event_parser.js'),
-            path = require('path');
+            path = require('path'),
+          mkdirp = require('mkdirp'),
+              fs = require('fs');  
 
-event_parser.read(path.join(__dirname, '..', 'events'), function(err, events) {
-  if (err) {
-    process.stderr.write("fatal error: "  + err + "\n");
-    process.exit(1);
-  }
-  console.log(JSON.stringify(events, null, "   "));
+const eventsDir = path.join(__dirname, '..', 'events'),
+        dataDir = path.join(__dirname, '..', 'html', 'data');
+
+mkdirp(dataDir, function(err) {
+  event_parser.read(eventsDir, function(err, events) {
+    if (err) {
+      process.stderr.write("fatal error: "  + err + "\n");
+      process.exit(1);
+    }
+    fs.writeFile(path.join(dataDir, "1.json"), JSON.stringify(events));
+  });
 });
